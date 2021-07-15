05c53e0f516d58b5af5ff3d4a324ef096f4cdd2d
Spacecreater <hirikomu@yandex.ru>
2021-07-15 16:53:02 +0300

    fix1

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 
 session_start();
-require_once __DIR__ . '/vendor/autoload.php';
+require_once(realpath(dirname(__FILE__) . 'C:\OpenServer\domains\localhost\form\vendor\autoload.php'));
 require_once __DIR__ . '/app/data.php';
 require_once __DIR__ . '/app/functions.php';
 
